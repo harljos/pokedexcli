d0adf37485a44452eca79b3722aa29363384c7ca
unknown <joseph.harley02@gmail.com>
2024-02-19 11:25:01 -0500

    created http request to put location areas in a struct

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/harljos/pokedexcli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeapiClient := pokeapi.NewClient()
+
+	resp, err := pokeapiClient.ListLocationAreas()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp)
+	// startRepl()
 }
